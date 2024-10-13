8b7ea923f53b9a8919b421ddb836e096fc5d83c1
Aleksandre Kapanadze <aleksandre.kapanadze@space.ge>
2024-10-13 12:09:33 +0400

    IAmTimCorey | Binary in C# | Initial

---
+++ IAmTimCorey/Binary in C#/BinaryInCSharpDemo/BinaryInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1001
+int binaryValue = 0b1001;
+string stringRepresentation = Convert.ToString(binaryValue, 2).PadLeft(32, '0');
+
+Console.WriteLine($"The original value is {binaryValue} (base 10)");
+Console.WriteLine($"The original value is {ConvertToBinary(binaryValue)} (base 2)");
+
+binaryValue = binaryValue << 3;
+
+Console.WriteLine($"The new value is {binaryValue} (base 10)");
+Console.WriteLine($"The new value is {ConvertToBinary(binaryValue)} (base 2)");
+
+string ConvertToBinary(int value, int places = 8)
+{
+    return Convert.ToString(value, 2).PadLeft(places, '0');
+}
