c223a8ddf19dd6685c60b12c59aa411c25182546
Aleksandre Kapanadze <aleksandre.kapanadze@outlook.com>
2024-12-06 12:23:37 +0400

    Udemy | Data Structures and Algorithms in C#

---
+++ Udemy/DataStructuresAndAlgorithmsInCSharp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms_DataStruct_Lib;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 23, 41, 25, 54, 18, 14 };
+            Sorting.BubbleSort(array);
+            array.ToList().ForEach(x => Console.Write(x + " "));
+
+            Console.Read();
+        }
+    }
+}
